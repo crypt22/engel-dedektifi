f35dde8f8040940374a46759979102b32477a1d5
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-11-27 11:57:54 +0200

    ioc configuration

---
+++ src/EEE/EEE/App_Start/IocConfig.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+using EEE.Controllers;
+using EEE.Domain.Repositories;
+using EEE.Domain.Services;
+
+namespace EEE.App_Start
+{
+    public class WindsorControllerFactory : DefaultControllerFactory
+    {
+        private readonly IKernel _kernel;
+        public WindsorControllerFactory(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public override void ReleaseController(IController controller)
+        {
+            _kernel.ReleaseComponent(controller);
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            if (controllerType == null)
+            {
+                throw new HttpException(404, string.Format("Controller for '{0}' could not be found.", requestContext.HttpContext.Request.Path));
+            }
+
+            return (IController)_kernel.Resolve(controllerType);
+        }
+    }
+
+    public class ControllersInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Types.FromThisAssembly().Pick()
+                                .If(Component.IsInSameNamespaceAs<BaseController>())
+                                .If(t => t.Name.EndsWith("Controller"))
+                                .Configure(configurer => configurer.Named(configurer.Implementation.Name))
+                                .LifestylePerWebRequest());
+        }
+    }
+
+    public class ServiceInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<IFormsAuthenticationService>().ImplementedBy<FormsAuthenticationService>().LifestylePerWebRequest());
+            container.Register(Component.For(typeof(IEntityRepository<>)).ImplementedBy(typeof(EntityRepository<>)).LifestylePerWebRequest());
+        }
+    }
+}
--- src/EEE/EEE/Global.asax.cs
+++ src/EEE/EEE/Global.asax.cs
@@ -1,14 +1,40 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Routing;
+
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Options;
+using MongoDB.Bson.Serialization.Serializers;
+
+using EEE.App_Start;
 
 namespace EEE
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            MvcHandler.DisableMvcResponseHeader = true;
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            var serializer = new DateTimeSerializer(DateTimeSerializationOptions.LocalInstance);
+            BsonSerializer.RegisterSerializer(typeof(DateTime), serializer);
+
+            var container = new WindsorContainer().Install(FromAssembly.This());
+            ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container.Kernel));
+        }
+
+        protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.Headers.Remove("X-Powered-By");
+            HttpContext.Current.Response.Headers.Remove("X-AspNet-Version");
+            HttpContext.Current.Response.Headers.Remove("X-AspNetMvc-Version");
+            HttpContext.Current.Response.Headers.Set("Server", "Web Server 1");
         }
     }
 }
